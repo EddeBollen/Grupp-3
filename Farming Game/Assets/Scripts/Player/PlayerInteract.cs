622f6ab55f1f57611e27a4998c8ea6648a1d283d
ArvidOlaussonHoltenas <arvid.olaussonholtenas@elev.ga.lbs.se>
2025-02-03 14:46:41 +0100

    npc + sounds + amazing grid system
    drgiuasrefgyh8asdfibuasdfui

--- Farming Game/Assets/Scripts/Player/PlayerInteract.cs
+++ Farming Game/Assets/Scripts/Player/PlayerInteract.cs
@@ -7,18 +7,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 {
-    private InputAction interact;
-    void Start()
-    {
-        
-    }
+    Vector2 gridPosition;
+    Vector2 mousePos;
+    float gridDistance;
+    [SerializeField] GameObject playerObject;
+    Collider2D hit;
+    public AudioSource audioSource;
     void OnInteract()
     {
-        Debug.Log("uisfgdaiusdf");
+        if (hit != null)
+        {
+            if (GridDistance(playerObject, hit.gameObject) <= 2f)
+            {
+                Debug.Log("Interact success, interacted with " + hit.gameObject.name + "!");
+                audioSource.Play();
+            }
+            else
+            {
+                Debug.Log("Too far away to interact!");
+            }
+        }
     }
 
-    void Update()
+    void OnFire()
     {
+        CalcGrid(mousePos);
+    }
 
+    Vector2 CalcGrid(Vector3 instance)
+    {
+        gridPosition = new Vector2(Mathf.Round(instance.x), Mathf.Round(instance.y));
+        Debug.Log(gridPosition);
+        return gridPosition;
+    }
+
+    float GridDistance(GameObject a, GameObject b)
+    {
+        Vector2 posA = CalcGrid(a.transform.position);
+        Vector2 posB = CalcGrid(b.transform.position);
+        Vector2 distance = new Vector2(Mathf.Max(posA.x, posB.x) - Mathf.Min(posA.x, posB.x), Mathf.Max(posA.y, posB.y) - Mathf.Min(posA.y, posB.y));
+        gridDistance = Mathf.Max(distance.x, distance.y);
+        Debug.Log(gridDistance);
+        return gridDistance;
+    }
+        
+    private void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        hit = Physics2D.OverlapPoint(mousePos);
     }
 }
