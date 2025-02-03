8d8f8f8ad30da52fab98b64b0474c95164e0f229
LeonJonik <leon.jonikic@elev.ga.lbs.se>
2025-02-03 11:29:42 +0100

    Small change Water

---
+++ Farming Game/Assets/Scripts/Player/PlantFuntions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantFuntions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnWater()
+    {
+        Debug.Log("Water");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
