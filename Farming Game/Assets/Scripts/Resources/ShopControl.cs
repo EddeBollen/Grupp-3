98277d479b34fc6908206f64b959154ae782d5d0
AdisBrkic08 <adisb55@gmail.com>
2025-02-10 13:06:18 +0100

    Shop Control Fixed

--- Farming Game/Assets/Scripts/Resources/ShopControl.cs
+++ Farming Game/Assets/Scripts/Resources/ShopControl.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class ShopControl : MonoBehaviour
 {
     int moneyAmount;
     int isAxeSold;
 
     public Text moneyAmountText;
-    public Text AxeSold;
+    public Text axePrice;
 
     public Button buyButton;
 
@@ -21,11 +22,11 @@ public class ShopControl : MonoBehaviour
 
     void Update()
     {
-        moneyAmountText.text = "Money: " + moneyAmount.ToString() + "$";
+           moneyAmountText.text = "Money: " + moneyAmount.ToString() + "$";  
 
         isAxeSold = PlayerPrefs.GetInt("isAxeSold");
 
-        if (moneyAmount > 5 && isAxeSold == 0)
+        if (moneyAmount > 50 && isAxeSold == 0)
         {
             buyButton.interactable = true;
         }
@@ -33,7 +34,16 @@ public class ShopControl : MonoBehaviour
     }
     public void buyAxe()
     {
-        moneyAmount -= 5;
-
+        moneyAmount -= 50;
+        PlayerPrefs.SetInt("IsAxeSold", 1);
+        axePrice.text = "Sold";
+        buyButton.gameObject.SetActive(false);
     }
+
+    public void exitShop()
+    {
+        PlayerPrefs.SetInt("MoneyAmount", moneyAmount);
+        SceneManager.LoadScene("SampleScene");
+    }
+   
 }
